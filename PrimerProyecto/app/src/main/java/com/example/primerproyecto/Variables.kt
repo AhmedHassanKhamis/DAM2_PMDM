5424cccce62cbf119acbe92cbc4be4482cacf970
ahmedhassank <ahmedmario@hotmail.es>
2024-10-02 22:07:07 +0200

    subo mi proyecto

---
+++ PrimerProyecto/app/src/main/java/com/example/primerproyecto/Variables.kt
@@ -0,0 +1,28 @@
+package com.example.primerproyecto
+
+fun main(){
+
+    //val Float
+    val ejemploFloat:Float = 0.34f
+
+    //Double
+    val ejemploDouble:Double = 432423.923
+
+    /**
+     *
+     * Variables Alfanumericas
+     *
+     */
+
+    //Char (entre comillas simples, un unico caracter)
+
+    val caracterChar:Char = '@'
+    val caracterChar2:Char = '2'
+    val caracterChar3:Char = 'r'
+
+    //String
+    val stringEjemplo:String = "hola mundo"
+
+    println(stringEjemplo)
+
+}
