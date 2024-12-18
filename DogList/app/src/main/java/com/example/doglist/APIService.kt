e968a62cf10d72165e7291ecf974355ef0a6b23d
usertar <ahmedmario@hotmail.es>
2024-12-18 21:06:06 +0100

    cambios en el doglist

--- DogList/app/src/main/java/com/example/doglist/APIService.kt
+++ DogList/app/src/main/java/com/example/doglist/APIService.kt
@@ -1,9 +1,10 @@
 package com.example.doglist
 
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Url
 
 interface APIService {
     @GET
-    fun getDogsPorRazas(@Url)
+    fun getDogsByBreed(@Url  url:String):Response<DogResponse>
 }
--- DogList/app/src/main/java/com/example/doglist/MainActivity.kt
+++ DogList/app/src/main/java/com/example/doglist/MainActivity.kt
@@ -6,8 +6,13 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.doglist.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
-private lateinit var binding:
+private lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,11 +21,37 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
 
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        initRecyclerView()
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+
     }
+
+    private fun initRecyclerView() {
+
+    }
+
+    private fun getRetrofit():Retrofit{
+        return Retrofit.Builder()
+            .baseUrl("https://dog.ceo/api/breed/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    private fun searchByName(query: String){
+        //creamos una corrutina para que todo_ lo que se llame aqui se ejecute en el hilo pricipal
+        CoroutineScope(Dispatchers.IO).launch {
+
+        }
+    }
+
 }
